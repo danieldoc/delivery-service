f0fc806d07e5afdf17e5e29ba1fc5b3ae939123d
Daniel Cruz <daniel.olicruz13@gmail.com>
2025-07-10 21:40:55 -0300

    feat: Configured shipment repository

---
+++ delivery-service-repository/src/main/java/br/com/danieldoc/deliveryservice/repository/ShipmentRepository.java
@@ -0,0 +1,14 @@
+package br.com.danieldoc.deliveryservice.repository;
+
+import br.com.danieldoc.deliveryservice.domain.entity.Shipment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
+
+    @Query("from Shipment s where s.code = :code and s.deleted = false")
+    Optional<Shipment> findByCodeAndNotDeleted(@Param("code") String code);
+}
