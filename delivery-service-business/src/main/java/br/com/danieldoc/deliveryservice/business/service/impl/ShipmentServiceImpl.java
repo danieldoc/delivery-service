8358ac9fa66d3434c10f6c568208c1f9a75f78e3
Daniel Cruz <daniel.olicruz13@gmail.com>
2025-07-10 23:07:08 -0300

    refactor: Implemented logs for ShipmentController and ShipmentServiceImpl

--- delivery-service-business/src/main/java/br/com/danieldoc/deliveryservice/business/service/impl/ShipmentServiceImpl.java
+++ delivery-service-business/src/main/java/br/com/danieldoc/deliveryservice/business/service/impl/ShipmentServiceImpl.java
@@ -5,14 +5,12 @@ import br.com.danieldoc.deliveryservice.domain.entity.Shipment;
 import br.com.danieldoc.deliveryservice.domain.exception.EntityNotFoundException;
 import br.com.danieldoc.deliveryservice.repository.ShipmentRepository;
 import jakarta.transaction.Transactional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ShipmentServiceImpl implements ShipmentService {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(ShipmentServiceImpl.class);
 
     private final ShipmentRepository shipmentRepository;
 
@@ -22,36 +20,36 @@ public class ShipmentServiceImpl implements ShipmentService {
 
     @Override
     public Shipment getDetail(String code) {
-        LOGGER.info("Start find shipment by code={}", code);
+        log.info("Start find shipment by code={}", code);
 
         final Shipment shipment = shipmentRepository.findByCodeAndNotDeleted(code)
                 .orElseThrow(EntityNotFoundException::new);
 
-        LOGGER.info("End find shipment by code={}", code);
+        log.info("End find shipment by code={}", code);
         return shipment;
     }
 
     @Transactional
     @Override
     public Shipment save(Shipment shipment) {
-        LOGGER.info("Start save shipment");
+        log.info("Start save shipment");
 
         shipment = shipmentRepository.save(shipment);
 
-        LOGGER.info("End save shipment with id={}", shipment.getId());
+        log.info("End save shipment with id={}", shipment.getId());
         return shipment;
     }
 
     @Transactional
     @Override
     public void deleteByCode(String code) {
-        LOGGER.info("Start delete shipment by code={}", code);
+        log.info("Start delete shipment by code={}", code);
 
         final Shipment shipment = getDetail(code);
 
         shipment.deleteIt();
         shipmentRepository.save(shipment);
 
-        LOGGER.info("End delete shipment by code={}", code);
+        log.info("End delete shipment by code={}", code);
     }
 }
--- delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/api/v1/controller/ShipmentController.java
+++ delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/api/v1/controller/ShipmentController.java
@@ -7,6 +7,7 @@ import br.com.danieldoc.deliveryservice.restapi.api.v1.request.ShipmentRequest;
 import br.com.danieldoc.deliveryservice.restapi.api.v1.response.ShipmentResponse;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 
+@Slf4j
 @RequestMapping("/api/v1/shipments")
 @RestController
 public class ShipmentController {
@@ -29,13 +31,20 @@ public class ShipmentController {
 
     @GetMapping("/{code}")
     public ShipmentResponse getDetail(@NotBlank @PathVariable String code) {
+        log.info("Start getDetail with code={}", code);
+
         final Shipment shipment = shipmentService.getDetail(code);
-        return shipmentMapper.toResponse(shipment);
+        final ShipmentResponse response = shipmentMapper.toResponse(shipment);
+
+        log.info("End getDetail with code={}", code);
+        return response;
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ResponseEntity<ShipmentResponse> create(@Valid @RequestBody ShipmentRequest shipmentRequest) {
+        log.info("Start create shipment");
+
         Shipment shipment = shipmentMapper.toShipment(shipmentRequest);
         shipment = shipmentService.save(shipment);
 
@@ -46,6 +55,7 @@ public class ShipmentController {
                 .buildAndExpand(createdShipment.getCode())
                 .toUri();
 
+        log.info("End create shipment with code={}", createdShipment.getCode());
         return ResponseEntity
                 .created(locationUri)
                 .body(createdShipment);
@@ -54,15 +64,24 @@ public class ShipmentController {
     @PutMapping("/{code}")
     public ShipmentResponse update(@NotBlank @PathVariable String code,
                                    @Valid @RequestBody ShipmentRequest shipmentRequest) {
+        log.info("Start update shipment with code={}", code);
+
         Shipment shipment = shipmentService.getDetail(code);
         shipment = shipmentMapper.updateFields(shipmentRequest, shipment);
         shipment = shipmentService.save(shipment);
-        return shipmentMapper.toResponse(shipment);
+        final ShipmentResponse response = shipmentMapper.toResponse(shipment);
+
+        log.info("End update shipment with code={}", code);
+        return response;
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{code}")
     public void delete(@NotBlank @PathVariable String code) {
+        log.info("Start delete with code={}", code);
+
         shipmentService.deleteByCode(code);
+
+        log.info("End delete with code={}", code);
     }
 }
