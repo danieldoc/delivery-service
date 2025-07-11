86fcf311707caeb41de339c260ac04cc5ebcd681
Daniel Cruz <daniel.olicruz13@gmail.com>
2025-07-10 21:46:43 -0300

    feat: Configured rest-api module

---
+++ delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/DeliveryServiceRestApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.danieldoc.deliveryservice.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryServiceRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceRestApiApplication.class, args);
+    }
+}
---
+++ delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/config/DeliveryServiceRestApiConfig.java
@@ -0,0 +1,9 @@
+package br.com.danieldoc.deliveryservice.restapi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("br.com.danieldoc.deliveryservice")
+@Configuration
+public class DeliveryServiceRestApiConfig {
+}
