6ad44cc418c722a0cd8b962bb65612d40707c4c4
Daniel Cruz <daniel.olicruz13@gmail.com>
2025-07-11 23:36:18 -0300

    refactor: Added super constructors at DeliveryServiceBusinessException

--- delivery-service-domain/src/main/java/br/com/danieldoc/deliveryservice/domain/exception/DeliveryServiceBusinessException.java
+++ delivery-service-domain/src/main/java/br/com/danieldoc/deliveryservice/domain/exception/DeliveryServiceBusinessException.java
@@ -1,4 +1,16 @@
 package br.com.danieldoc.deliveryservice.domain.exception;
 
 public class DeliveryServiceBusinessException extends RuntimeException {
+
+    public DeliveryServiceBusinessException(String message) {
+        super(message);
+    }
+
+    public DeliveryServiceBusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeliveryServiceBusinessException(Throwable cause) {
+        super(cause);
+    }
 }
--- delivery-service-domain/src/main/java/br/com/danieldoc/deliveryservice/domain/exception/EntityNotFoundException.java
+++ delivery-service-domain/src/main/java/br/com/danieldoc/deliveryservice/domain/exception/EntityNotFoundException.java
@@ -1,4 +1,16 @@
 package br.com.danieldoc.deliveryservice.domain.exception;
 
 public class EntityNotFoundException extends DeliveryServiceBusinessException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+    public EntityNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EntityNotFoundException(Throwable cause) {
+        super(cause);
+    }
 }
