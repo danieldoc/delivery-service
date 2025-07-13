ba1cfc057ad9d4f34ecd806af96e481cd2275a9d
Daniel Cruz <daniel.olicruz13@gmail.com>
2025-07-13 15:05:04 -0300

    feat: Set default TimeZone to UTC

--- delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/DeliveryServiceRestApiApplication.java
+++ delivery-service-rest-api/src/main/java/br/com/danieldoc/deliveryservice/restapi/DeliveryServiceRestApiApplication.java
@@ -3,10 +3,13 @@ package br.com.danieldoc.deliveryservice.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class DeliveryServiceRestApiApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(DeliveryServiceRestApiApplication.class, args);
     }
 }
